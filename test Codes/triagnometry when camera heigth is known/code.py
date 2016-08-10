dd242abf7222f5c1bea49e82394493788de66fef
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2016-08-10 18:08:38 +0530

    completed the code

--- test Codes/triagnometry when camera heigth is known/code.py
+++ test Codes/triagnometry when camera heigth is known/code.py
@@ -2,26 +2,6 @@ import numpy as np
 import cv2
 import math
 
-def distance_to_camera(imageHeigth,imageWidth,cameraHeigth, pitchAngle, fovv, fovh, focalLength, perWidth):
-    # compute and return the distance from the maker to the camera
-    verticalAngle = pitchAngle + (imageHeigth/2 - perWidth[1])*(fovv/imageHeigth)
-    rotationAngle = (perWidth[0]- imageWidth/2)*(fovh/imageWidth)
-    y = cameraHeigth*math.tan(verticalAngle)
-    x = y*math.tan(rotationAngle)
-    z = math.sqrt(cameraHeigth**2 + y**2 + x**2)
-    return z
-
-def getPoints():
-    return [300,300]
-
-# initialize the known distance from the camera to the object, which
-# in this case is 24 inches
-KNOWN_DISTANCE = 24.0
- 
-# initialize the known object width, which in this case, the piece of
-# paper is 11 inches wide
-KNOWN_WIDTH = 11.0
-
 #initializing the camera heigth
 CAMERA_HEIGTH = 48.0
 
@@ -34,26 +14,32 @@ FOVV = math.radians(90)
 #initializing field of view in horizontal direction
 FOVH = math.radians(90)
 
+#focalLength of the camera
+FOCAL_LENGTH = 543;
+
+#main function with the logic implemented
+def distance_to_camera(imageHeigth,imageWidth,cameraHeigth, pitchAngle, fovv, fovh, focalLength, perWidth):
+    verticalAngle = pitchAngle + (imageHeigth/2 - perWidth[1])*(fovv/imageHeigth)
+    rotationAngle = (perWidth[0]- imageWidth/2)*(fovh/imageWidth)
+    y = cameraHeigth*math.tan(verticalAngle)
+    x = y*math.tan(rotationAngle)
+    z = math.sqrt(cameraHeigth**2 + y**2 + x**2)
+    return z
+
+#we are calculating distance to this point from the camera
+def getPoints():
+    return [300,300]
 
 # initialize the list of images that we'll be using
 IMAGE_PATHS = ["images/2ft.png", "images/3ft.png", "images/4ft.png"]
 
-# load the furst image that contains an object that is KNOWN TO BE 2 feet
-# from our camera, then find the paper marker in the image, and initialize
-# the focal length
-image = cv2.imread(IMAGE_PATHS[0])
-focalLength = 543;
-print focalLength
-
-
 # loop over the images
 for imagePath in IMAGE_PATHS:
-    # load the image, find the marker in the image, then compute the
-    # distance to the marker from the camera
     image = cv2.imread(imagePath)
     height, width,_ = image.shape
-    inches = distance_to_camera(height,width,CAMERA_HEIGTH,PITCH_ANGLE,FOVV,FOVH, focalLength, getPoints())
+    inches = distance_to_camera(height,width,CAMERA_HEIGTH,PITCH_ANGLE,FOVV,FOVH, FOCAL_LENGTH, getPoints())
  
+	#displaying results
     cv2.circle(image,(300,300), 63, (0,0,255), -1)
     cv2.putText(image, "%.2fft" % (inches / 12),
         (image.shape[1] - 200, image.shape[0] - 300), cv2.FONT_HERSHEY_SIMPLEX,
