2bc7ebc2de9b75bc91717127566f0d47bd27d842
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2016-08-10 23:16:56 +0530

    complete code

--- test Codes/triagnometry when camera heigth is known/code.py
+++ test Codes/triagnometry when camera heigth is known/code.py
@@ -14,36 +14,32 @@ FOVV = math.radians(90)
 #initializing field of view in horizontal direction
 FOVH = math.radians(90)
 
-#focalLength of the camera
-FOCAL_LENGTH = 543;
-
-#main function with the logic implemented
+focalLength = 543;
+global mouse_x,mouse_y
+mouse_x = 300
+mouse_y = 300
 def distance_to_camera(imageHeigth,imageWidth,cameraHeigth, pitchAngle, fovv, fovh, focalLength, perWidth):
     verticalAngle = pitchAngle + (imageHeigth/2 - perWidth[1])*(fovv/imageHeigth)
     rotationAngle = (perWidth[0]- imageWidth/2)*(fovh/imageWidth)
     y = cameraHeigth*math.tan(verticalAngle)
     x = y*math.tan(rotationAngle)
     z = math.sqrt(cameraHeigth**2 + y**2 + x**2)
-    return z
+    return x,y,z
 
-#we are calculating distance to this point from the camera
 def getPoints():
-    return [300,300]
+    return [200,300]
 
-# initialize the list of images that we'll be using
-IMAGE_PATHS = ["images/2ft.png", "images/3ft.png", "images/4ft.png"]
-
-# loop over the images
-for imagePath in IMAGE_PATHS:
-    image = cv2.imread(imagePath)
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('image')
+while(True):
+    ret, image = cap.read()
     height, width,_ = image.shape
-    inches = distance_to_camera(height,width,CAMERA_HEIGTH,PITCH_ANGLE,FOVV,FOVH, FOCAL_LENGTH, getPoints())
- 
-	#displaying results
-    cv2.circle(image,(300,300), 63, (0,0,255), -1)
-    cv2.putText(image, "%.2fft" % (inches / 12),
-        (image.shape[1] - 200, image.shape[0] - 300), cv2.FONT_HERSHEY_SIMPLEX,
-        2.0, (0, 255, 0), 3)
+    x,y,z = distance_to_camera(height,width,CAMERA_HEIGTH,PITCH_ANGLE,FOVV,FOVH, focalLength, getPoints())
+    cv2.circle(image,(mouse_x,mouse_y), 63, (0,0,255), -1)
     cv2.imshow("image", image)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
+    print "x= %.2f y= %.2ft z= %.2ft" %(x/12,y/12,z/12)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
